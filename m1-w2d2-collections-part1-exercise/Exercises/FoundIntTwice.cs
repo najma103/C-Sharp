5b976fb979b4532a667cea814436551a93bf8323
Najma Aden <najma.aden@gmail.com>
2017-02-07 11:41:17 -0500

    Add files via upload

---
+++ m1-w2d2-collections-part1-exercise/Exercises/FoundIntTwice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /* 
+         Given a List of Integers, and an int value, return true if the int value appears two or more times in 
+         the list.
+         foundIntTwice( [5, 7, 9, 5, 11], 5 ) -> true
+         foundIntTwice( [6, 8, 10, 11, 13], 8 -> false
+         foundIntTwice( [9, 23, 44, 2, 88, 44], 44) -> true
+         */
+        public bool FoundIntTwice(List<int> integerList, int intToFind)
+        {
+            int count = 0;
+            foreach (int element in integerList)
+            {
+                if (element == intToFind)
+                {
+                    count += 1;
+                }
+            }
+            if (count >= 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+
+    }
+}
