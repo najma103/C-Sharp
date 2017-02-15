b89e6816e7930472d0e76f428b4a5f047f8adc7b
Najma Aden <najma.aden@gmail.com>
2017-02-15 11:35:02 -0500

    Add files via upload

---
+++ CalculatorExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechElevator.Classes;
+
+namespace CalculatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator newCalc = new Calculator(5);
+
+            Console.WriteLine("{0} plus 2 is {1}",
+                newCalc.Result, newCalc.Add(2));
+
+            Console.WriteLine("{0} Minus 2 is {1}",
+               newCalc.Result, newCalc.Subtract(2));
+
+            Console.WriteLine("{0} times 2 is {1}",
+               newCalc.Result, newCalc.Multiply(2));
+
+        }
+    }
+}
---
+++ CompanyExercise/Classes/Company.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechElevator.Classes
+{
+    public class Company
+    {
+        public string name;
+        /// <summary>
+        /// Gets and sets the name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int numberOfEmployees;
+        /// <summary>
+        /// Gets and sets the numberOfEmployees
+        /// </summary>
+        public int NumberOfEmployees
+        {
+            get { return numberOfEmployees; }
+            set { numberOfEmployees = value; }
+        }
+        public decimal revenue;
+        /// <summary>
+        /// Gets and sets the revenue
+        /// </summary>
+        public decimal Revenue
+        {
+            get { return revenue; }
+            set { revenue = value; }
+        }
+        public decimal expenses;
+        /// <summary>
+        /// Gets and sets the expenses
+        /// </summary>
+        public decimal Expenses
+        {
+            get { return expenses; }
+            set { expenses = value; }
+        }
+
+        /// company size - returns string
+        public string GetCompanySize()
+        {
+            if (numberOfEmployees > 250)
+            {
+                return "large";
+            }
+            else if (numberOfEmployees >= 50 && numberOfEmployees <= 250)
+            {
+                return "medium";
+            }
+            else
+            {
+                return "small";
+            }
+        }
+        /// method calculates company provit and returns profit in decimal
+        public decimal GetProfit()
+        {
+            return (revenue - expenses);
+        }
+    }/// end of class
+}
