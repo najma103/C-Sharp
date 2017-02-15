d38ffc4feeff6a79f136c824e7d057fd3e4625f2
Najma Aden <najma.aden@gmail.com>
2017-02-15 11:20:44 -0500

    added shopping cart and product project.

---
+++ ProductExercise/Classes/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechElevator.Classes
+{
+    public class Product
+    {
+        public string name;
+        /// <summary>
+        /// Gets and sets the name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal price;
+        /// <summary>
+        /// Gets and sets the price
+        /// </summary>
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public double weightInOunces;
+        /// <summary>
+        /// Gets and sets the weightInOunces
+        /// </summary>
+        public double WeightInOunces
+        {
+            get { return weightInOunces; }
+            set { weightInOunces = value; }
+        }
+    }
+}
---
+++ ShoppingCartExercise/Classes/ShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechElevator.Classes
+{
+    /// <summary>
+    /// A shopping cart class stores items in it.
+    /// </summary>
+    public class ShoppingCart
+    {
+        public int totalNumberOfItems;
+        public double totalAmountOwed;
+
+        public double GetAveragePricePerItem()
+        {
+            if (totalNumberOfItems > 0 && totalAmountOwed > 0)
+            {
+                double avgPricePerItem =
+               (double)(totalAmountOwed / totalNumberOfItems);
+                return avgPricePerItem;
+            } else
+            {
+                return 0.0;
+            }
+
+        }
+        public void AddItems(int numberOfItems, double pricePerItem)
+        {
+            totalNumberOfItems += numberOfItems;
+            totalAmountOwed = totalNumberOfItems * pricePerItem;
+        }
+
+        public void Empty()
+        {
+            totalNumberOfItems = 0;
+            totalAmountOwed = 0;
+        }
+
+    }
+}
